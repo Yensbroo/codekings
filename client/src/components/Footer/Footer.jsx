4f9d42d5e548e857805646a23213f8ecdcb963da
Yens Broothaers <yens.broothaers@student.arteveldehs.be>
2018-06-03 23:34:46 +0200

    New footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class componentName extends Component {
   render() {
-    return <footer>Test</footer>;
+    return (
+      <footer>
+        <img src="/logo/codekings_logo_wit.svg" alt="" />
+        <Link to="https://github.com/Yensbroo">© Yens Broothaers</Link>
+      </footer>
+    );
   }
 }
