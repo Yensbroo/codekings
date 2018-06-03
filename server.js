8fd63ad4217200b7837976b69a63f2bca2ceb00f
Yens Broothaers <yens.broothaers@student.arteveldehs.be>
2018-06-03 19:18:09 +0200

    Ready for deploy

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ const routes = require("./server/routes");
 const app = express();
 
 // express middleware
-app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(express.static(path.join(__dirname, 'client/public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -39,6 +38,14 @@ app.use(auth.initialize());
 
 //Routes
 app.use("", routes);
+
+if(process.env.NODE_ENV === 'production') {
+        app.use(express.static('client/build'));
+
+        app.get('*', (req, res) => {
+                res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        })
+}
 
 
 const port = process.env.PORT || 8080;
